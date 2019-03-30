13352b5e4a8b196f016d300e32e933360e6b280f
Jin <jin.s@onther.io>
2019-03-30 17:26:35 +0000

    Fixed, User node has same genesis which on operator's

--- docker.user.pls.sh
+++ docker.user.pls.sh
@@ -27,7 +27,11 @@ geth \
   --rpcaddr 0.0.0.0 \
   --rpcport 8547 \
   --port 30307 \
-  --rootchain.url $ROOTCHAIN_ADDR:8546 \
-  --rootchain.contract $ROOTCHAIN \
+  --dev \
+  --dev.p2p \
+  --dev.operator 0x71562b71999873DB5b286dF957af199Ec94617F7 \
+  --dev.key b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291 \
+  --rootchain.url "ws://${ROOTCHAIN_ADDR}:8546" \
+  --dev.rootchain $ROOTCHAIN \
   --syncmode "full" \
-  --bootnodes "enode://401bd6383fe11a5224d5b4277b53d7c0278efed3ca685b6593935751ad1fe734a8e35d2b3ebd9d7fc6da6cff12e72cfcfca8db408bf2e49f1fad4c503956d07f@${BOOTNODE_ADDR}:30301" > log/user_node.log
+  --bootnodes "enode://401bd6383fe11a5224d5b4277b53d7c0278efed3ca685b6593935751ad1fe734a8e35d2b3ebd9d7fc6da6cff12e72cfcfca8db408bf2e49f1fad4c503956d07f@${BOOTNODE_ADDR}:30301" &> log/user_node.log
